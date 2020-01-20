5628ca9934431fcc7811a60dac7e9e0c06bade6a
Ben Proctor <ben@satorilab.org>
2020-01-20 18:22:03 +0000

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQVdfSZS8Xvf7KHVnYRtH2YwftTWoXYsNWDRPSlVqpcfUhBQeG1O5bQQtp6DRKNz95cP3I242oymWAs/pubhtml';
